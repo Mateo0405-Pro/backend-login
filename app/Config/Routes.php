faa46300b50bafb8694a2b15b96340ad6e81378a
Mateo Perugachi <matheoperugachi3@gmailo.com>
2025-04-29 09:56:36 -0500

    Cambio en ruta: se modifico de GET a POST

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,9 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->post('api/register', 'AuthController::register');
-//$routes->get('api/register', 'AuthController::register');
-
-
+$routes->get ('api/register', 'AuthController::register');
 $routes->post('api/login', 'AuthController::login');
 $routes->get('api/protected', 'AuthController::protected');
